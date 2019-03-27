e37cac0bbe5c9d214b6e52ddf966f8b648bc772f
Lukasz Winklarz <lukasz.winklarz@gmail.com>
2019-03-27 21:58:02 +0100

    + Init of project

---
+++ src/main/java/com/example/testapp/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.testapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "hello")
+public class HelloWorldController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
